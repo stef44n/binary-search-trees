57ccbb2a493181b1b774a02aed8dcef37ff7bbf2
Stefan <stefanseifeddine@gmail.com>
2022-11-01 05:01:38 +0000

    Write up buildTree function to return nodes

--- script.js
+++ script.js
@@ -14,10 +14,51 @@ class Tree {
 
 function buildTree(anArray) {
     // console.log(anArray);
-    let uniqueChars = [...new Set(anArray)];
-    // console.log(uniqueChars);
-    let ordered = uniqueChars.sort((a, b) => a - b);
-    return ordered;
+    let mainNode = new Node(anArray); // make this root/0? add subnodes??
+    if (anArray.length === 0) {
+        return null;
+    } else {
+        let uniqueChars = [...new Set(anArray)];
+        // console.log(uniqueChars);
+        let ordered = uniqueChars.sort((a, b) => a - b);
+        // console.log(ordered);
+        if (ordered.length === 1) {
+            mainNode.data = ordered;
+            // console.log(mainNode.data);
+
+            return mainNode;
+        } else if (ordered.length > 0) {
+            let midIndex = Math.floor(ordered.length / 2);
+            // console.log(`mid index: floor.(${ordered.length}/2) = ${midIndex}`);
+
+            let midValue = ordered[midIndex];
+            console.log(`mid Value = ${midValue}`);
+
+            let leftArr = ordered.slice(0, midIndex);
+            // console.log(leftArr);
+
+            let rightArr = ordered.slice(midIndex + 1);
+            // console.log(rightArr);
+
+            let leftNode = new Node(leftArr);
+            leftNode.data = leftArr;
+            mainNode.left = buildTree(leftNode.data);
+            // buildTree(leftNode.data);
+
+            let rightNode = new Node(rightArr);
+            rightNode.data = rightArr;
+            mainNode.right = buildTree(rightNode.data);
+            // buildTree(rightNode.data);
+
+            mainNode.data = midValue;
+
+            // console.log(mainNode.data);
+            // console.log(leftNode.data);
+            // console.log(rightNode.data);
+            return mainNode;
+        }
+    }
+    return mainNode;
 }
 
 arr1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
@@ -25,5 +66,18 @@ arr1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 // console.log(buildTree(arr1));
 
 let aTree = new Tree(arr1);
-
 console.log(aTree);
+
+let aNode = new Node(arr1);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+prettyPrint(aTree.array);
